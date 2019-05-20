c0dc4bb06ee45fc6b3fbce3e6c9de52d99928a87
isjin <isjin1@163.com>
2019-05-21 04:19:35 +0800

    update

--- passwdgenerate.sh
+++ passwdgenerate.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 user=$1
 date=`date +%Y-%m-%d`
-passwd=`pwmake 20`
-echo "$date $user $passwd" >> log/user.txt
-echo "$passwd" >passwd_tmp
+path=~/usermanage
+
+record=`more $usermanager_path/log/user.txt | grep test | tail -n 1 | awk '{print $3}'| wc -l`
+if [ $record -gt 0 ];then
+	password=`more $usermanager_path/log/user.txt | grep test | tail -n 1 | awk '{print $3}'`
+	echo "$password" >passwd_tmp
+else
+	password=`pwmake 20`
+	echo "$date $user $password" >> log/user.txt
+	echo "$password" >passwd_tmp
+fi
