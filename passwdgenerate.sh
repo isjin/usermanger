47dca90e5e1777064551d79463b87ec3d2386c69
isjin <isjin1@163.com>
2019-08-21 14:52:24 +0800

    update

--- passwdgenerate.sh
+++ passwdgenerate.sh
@@ -3,12 +3,17 @@ user=$1
 date=`date +%Y-%m-%d`
 path=~/usermanage
 
-record=`more $usermanager_path/log/user.txt | grep test | tail -n 1 | awk '{print $3}'| wc -l`
-if [ $record -gt 0 ];then
-	password=`more $usermanager_path/log/user.txt | grep test | tail -n 1 | awk '{print $3}'`
-	echo "$password" >passwd_tmp
-else
-	password=`pwmake 20`
-	echo "$date $user $password" >> log/user.txt
-	echo "$password" >passwd_tmp
-fi
+# record=`more $path/log/user.txt | grep $user | tail -n 1 | awk '{print $3}'| wc -l`
+# if [ $record -gt 0 ];then
+	# password=`more $path/log/user.txt | grep $user | tail -n 1 | awk '{print $3}'`
+	# echo "$password" >passwd_tmp
+# else
+	# password=`pwmake 20`
+	# echo "$date $user $password" >> log/user.txt
+	# echo "$password" >passwd_tmp
+# fi
+
+
+password=`pwmake 20`
+echo "$date $user $password" >> log/user.txt
+echo "$password" >passwd_tmp
---
+++ usermanger_other.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#./usermanger.sh test linux local add
+user=$1
+system=$2
+environment=$3
+operator=$4
+permission=$5
+path=~/usermanage
+
+
+if [ $system == "windows" ];then
+##windows##
+	if [ $operator == "add" ];then
+		bash $path/passwdgenerate.sh $user 2
+		password=`more $path/passwd_tmp`
+		if [ ! -d $path/ssh_key/$user ];then
+			mkdir -p $path/ssh_key/$user
+		fi
+		echo "Windows server login user infomation:" >> $path/ssh_key/$user/$user\.txt
+		tail -n 1 $path/log/user.txt >> ssh_key/$user/$user\.txt
+		sed "s/{hosts}/$environment/g" $path/playbook/adduser-win.yml > $path/$environment\-adduser-win.yml
+		sed -i "s/{user}/$user/g" $path/$environment\-adduser-win.yml
+		sed -i "s/{password}/$password/g" $path/$environment\-adduser-win.yml
+		sed -i "s/{group1}/$permission/g" $path/$environment\-adduser-win.yml
+		ansible-playbook $path/$environment\-adduser-win.yml
+		rm -rf $path/$environment\-adduser-win.yml
+		cd $path/ssh_key
+		zip $path/$user $user/*
+	else
+		sed "s/{hosts}/$environment/g" $path/playbook/deleteuser-win.yml > $path/$environment\-deleteuser-win.yml
+		sed -i "s/{user}/$user/g" $path/$environment\-deleteuser-win.yml
+		ansible-playbook $path/$environment\-deleteuser-win.yml
+		rm -rf $path/$environment\-deleteuser-win.yml
+	fi
+else
+	if [ $operator == "add" ];then
+		bash $path/sshkey_generate.sh $user 1
+		sed "s/{hosts}/$environment/g" $path/playbook/adduser-linux.yml > $path/$environment\-adduser-linux.yml
+		sed -i "s/{user}/$user/g" $path/$environment\-adduser-linux.yml
+		sed -i "s/{sshkey}/ssh_key\/$user\/$user\.pem.pub/g" $path/$environment\-adduser-linux.yml
+		ansible-playbook $path/$environment\-adduser-linux.yml
+		rm -rf $path/$environment\-adduser-linux.yml
+		if [ $permission == "sudo" ];then
+			sed "s/{hosts}/$environment/g" $path/playbook/sudoer.yml > $path/$environment\-sudoer.yml
+			sed -i "s/{user}/$user/g" $path/$environment\-sudoer.yml
+			sed -i "s/{sshkey}/ssh_key\/$user\/$user\.pem.pub/g" $path/$environment\-sudoer.yml
+			ansible-playbook $path/$environment\-sudoer.yml
+			rm -rf $path/$environment\-sudoer.yml
+		fi
+		cd $path/ssh_key
+		zip $path/$user $user/*
+	else
+		sed "s/{hosts}/$environment/g" $path/playbook/deleteuser-linux.yml > $path/$environment\-deleteuser-linux.yml
+		sed -i "s/{user}/$user/g" $path/$environment\-deleteuser-linux.yml
+		ansible-playbook $path/$environment\-deleteuser-linux.yml
+		rm -rf $path/$environment\-deleteuser-linux.yml
+	fi
+fi
+
